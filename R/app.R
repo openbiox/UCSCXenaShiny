16831d807e8c8c7fe7fe5b5817c27ec5cb78777a
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-03-31 15:48:22 +0800

    📦 NEW: 上传Xena相关数据

--- R/app.R
+++ R/app.R
@@ -5,7 +5,11 @@
 
 
 # Dependencies check ------------------------------------------------------
-pkgs <- c("shiny", "shinythemes", "magrittr", "UCSCXenaTools",
+
+# 暂时使用开发版本的UCSCXenaTools，第一次使用先取消下面注释进行安装
+# remotes::install_github("ShixiangWang/UCSCXenaTools", build_vignettes = TRUE)
+
+pkgs <- c("shiny", "shinythemes", "UCSCXenaTools",
           "echarts4r")
 for (pkg in pkgs){
   if (!require(pkg, character.only = TRUE)) {
@@ -20,6 +24,31 @@ rm(pkgs)
 
 # Here data goes
 data("XenaData", package = "UCSCXenaTools")
+View(XenaData)
+
+# xena_all = XenaData %>% XenaGenerate()
+# XenaInfo = list()
+# samps = list()
+# for (i in 1:nrow(XenaData)) {
+#   s = XenaData[i, ] %>% XenaGenerate() %>% samples(by = "datasets", how = "any")
+#   message("Processing #", i)
+#   samps[[i]] = s[[1]]
+# }
+# rm(s)
+# names(samps) = XenaData$XenaDatasets
+# saveRDS(samps, file = "data/XenaSamples.rds")
+# 
+# all_chr = purrr::map_lgl(samps, is.character)
+# XenaInfo$all_samples = purrr::reduce(samps[as.integer(which(all_chr))], union)
+# #XenaInfo$all_samples = Reduce(union, samps)
+# XenaInfo$n_samples = length(XenaInfo$all_samples)
+# XenaInfo$n_hubs = length(hosts(xena_all))
+# XenaInfo$n_cohorts = length(cohorts(xena_all))
+# XenaInfo$n_datasets = length(datasets(xena_all))
+# XenaInfo$all_samples = NULL
+# save(XenaInfo, file = "data/XenaInfo.RData")
+
+load(file = "data/XenaInfo.RData")
 
 # UI ----------------------------------------------------------------------
 
