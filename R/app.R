a52f475ebc3b8e8c827f0d387d8f7804b2305a8e
longfei8533 <37660840+longfei8533@users.noreply.github.com>
2019-04-03 12:26:48 +0800

    Zlf (#14)
    * add navbarPage
    
    * add developers page

--- R/app.R
+++ R/app.R
@@ -73,33 +73,147 @@ fun_download <- function(datasets, destdir=tempdir(),
 
 # UI ----------------------------------------------------------------------
 
-ui <- fluidPage(theme = shinytheme("spacelab"),
-                navbarPage('XenaShiny',
-                           tabPanel(title = "Home"), 
-                           tabPanel(title = 'Repository',
-                                    sidebarLayout(
-                                      sidebarPanel(
-                                        selectInput('side',
-                                                    'Select',
-                                                    choices = c('ab'='ab',
-                                                                'bc'='bc','cd'='cd'),selected = 'ab')
-                                      ),
-                                      mainPanel(DT::dataTableOutput("xena_table"))
-                                    )),
-                           navbarMenu(title = 'Analyses',
-                                      tabPanel('a'),
-                                      tabPanel('b'),
-                                      tabPanel('b')
-                           ),
-                           tabPanel(title = 'About',
-                                    includeMarkdown("md/about.md")),
-                           tags$footer(HTML("Openbiox &copy; MIT"), align = "center", style = "
-                            position:absolute;
-                            bottom:0;
-                            width:100%;
-                            height:50px;   /* Height of the footer */
-                            padding: 10px;
-                            z-index: 1000;"))
+ui = navbarPage(
+  shinythemes::themeSelector(),  # <--- Add this somewhere in the UI
+  
+  tabPanel(title="Home",
+           icon = icon("home") #create icon http://shiny.rstudio.com/reference/shiny/latest/icon.html
+           ),
+  tabPanel(title="Repository",
+           icon = icon("database"),
+           sidebarPanel(
+             textInput("txt", "Text input:", "text here"),
+             sliderInput("slider", "Slider input:", 1, 100, 30),
+             actionButton("action", "Button"),
+             actionButton("action2", "Button2", class = "btn-primary")
+           ),
+           mainPanel(
+             tabsetPanel(
+               tabPanel("Tab 1"),
+               tabPanel("Tab 2")
+             )
+           )
+  ),
+  tabPanel(title="Developers",
+           icon = icon("user-friends"),
+           fluidPage(
+             #titlePanel("Developers"),
+             fluidRow(
+               class ="text-center center-block",
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Shixiang Wang"),
+                                   tags$p(class="card-text","Some information displayed"),
+                                   tags$a(href="#",class="card-link","See Profile")
+                          )
+                 )
+               ),
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Fei Zhao"),
+                                   tags$p(class="card-text","Some information displayed"),
+                                   tags$a(href="#",class="card-link","See Profile")
+                          )
+                 )
+               ),
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Yi Xiong"),
+                                   tags$p(class="card-text","Some information displayed"),
+                                   tags$a(href="#",class="card-link","See Profile")
+                          )
+                 )
+               )),
+             tags$br(),
+             fluidRow(
+               class ="text-center center-block",
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Longfei Zhao"),
+                                   tags$p(class="card-text","Some information displayed")
+                          ),
+                          tags$div(class="card-footer",
+                                   tags$a(href="#",class="card-link","See Profile"))
+                 )
+               ),
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Yin Li"),
+                                   tags$p(class="card-text","Some information displayed"),
+                                   tags$a(href="#",class="card-link","See Profile")
+                          )
+                 )
+               ),
+               column(
+                 4,
+                 tags$div(class = "card bg-info text-dark center-block",
+                          style="width:400px",
+                          tags$img(class="card-img-top  img-circle img-responsive center-block",
+                                   src="people.png",alt="Card image"),
+                          tags$div(class="card-body",
+                                   tags$h4(class="card-title","Kai Gu"),
+                                   tags$p(class="card-text","Some information displayed"),
+                                   tags$a(href="#",class="card-link","See Profile")
+                                   )
+                          )
+               )
+             )
+           )
+  )
+
+ 
+# ui <- fluidPage(theme = shinytheme("spacelab"),
+#                 navbarPage('XenaShiny',
+#                            tabPanel(title = "Home"), 
+#                            tabPanel(title = 'Repository',
+#                                     sidebarLayout(
+#                                       sidebarPanel(
+#                                         selectInput('side',
+#                                                     'Select',
+#                                                     choices = c('ab'='ab',
+#                                                                 'bc'='bc','cd'='cd'),selected = 'ab')
+#                                       ),
+#                                       mainPanel(DT::dataTableOutput("xena_table"))
+#                                     )),
+#                            navbarMenu(title = 'Analyses',
+#                                       tabPanel('a'),
+#                                       tabPanel('b'),
+#                                       tabPanel('b')
+#                            ),
+#                            tabPanel(title = 'About',
+#                                     includeMarkdown("md/about.md")),
+#                            tags$footer(HTML("Openbiox &copy; MIT"), align = "center", style = "
+#                             position:absolute;
+#                             bottom:0;
+#                             width:100%;
+#                             height:50px;   /* Height of the footer */
+#                             padding: 10px;
+#                             z-index: 1000;"))
 )
 
 
