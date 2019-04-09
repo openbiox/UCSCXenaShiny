f1b7cd255b3b2bc28bffe06a9916f7f50221ea49
kaigu1990 <gukai1212@163.com>
2019-04-09 16:52:01 +0800

    add Repository page

--- R/app.R
+++ R/app.R
@@ -12,7 +12,7 @@
 
 pkgs <- c(
   "shiny", "shinythemes", "UCSCXenaTools",
-  "echarts4r", "DT"
+  "echarts4r", "DT", "shinyjs"
 )
 for (pkg in pkgs) {
   if (!require(pkg, character.only = TRUE)) {
@@ -55,11 +55,10 @@ colnames(xena_table)[c(1:3)] <- c("Dataset ID", "Hub", "Cohort")
 # XenaInfo$n_cohorts = length(cohorts(xena_all))
 # XenaInfo$n_datasets = length(datasets(xena_all))
 # XenaInfo$all_samples = NULL
-# XenaInfo$n_subtypes = length(names(table(XenaData$DataSubtype)))
-# XenaInfo$n_origin = length(names(table(XenaData$AnatomicalOrigin)))
 # save(XenaInfo, file = "data/XenaInfo.RData")
 
 load(file = "data/XenaInfo.RData")
+
 
 
 # Functions ---------------------------------------------------------------
@@ -78,179 +77,301 @@ fun_download <- function(datasets, destdir = tempdir(),
 
 # UI ----------------------------------------------------------------------
 
-ui <- navbarPage(
-  title = "",
-  # shinythemes::themeSelector(),  # <--- Add this somewhere in the UI
-  tabPanel(
-    title = "Home",
-    icon = icon("home") # create icon http://shiny.rstudio.com/reference/shiny/latest/icon.html
-  ),
-  tabPanel(
-    title = "Repository",
-    icon = icon("database"),
-    sidebarPanel(
-      textInput("txt", "Text input:", "text here"),
-      sliderInput("slider", "Slider input:", 1, 100, 30),
-      actionButton("action", "Button"),
-      actionButton("action2", "Button2", class = "btn-primary")
+library(shiny)
+library(shinyjs)
+
+ui <- tagList(
+  shinyjs::useShinyjs(),
+  
+  navbarPage(
+    title = "",
+    # shinythemes::themeSelector(),  # <--- Add this somewhere in the UI
+    tabPanel(
+      title = "Home",
+      icon = icon("home") # create icon http://shiny.rstudio.com/reference/shiny/latest/icon.html
     ),
-    mainPanel(DT::dataTableOutput("xena_table"))
-  ),
-  navbarMenu(
-    title = "Modules",
-    icon = icon("buromobelexperte"),
-    tabPanel("module 1"),
-    tabPanel("module 1"),
-    tabPanel("module 3")
-  ),
-  navbarMenu(
-    title = "Pipelines",
-    icon = icon("angle-double-down"),
-    tabPanel("pipeline 1"),
-    tabPanel("pipeline 2"),
-    tabPanel("pipeline 3")
-  ),
-  tabPanel(
-    title = "Developers",
-    icon = icon("user-friends"),
-    fluidPage(
-      # titlePanel("Developers"),
-      fluidRow(
-        class = "text-center center-block",
-        column(
-          4,
+    tabPanel(
+      title = "Repository",
+      icon = icon("database"),
+      
+      sidebarLayout(
+        sidebarPanel(
+          hr(),
+          actionLink("hubs", "Active Data Hubs :", icon = icon("arrow-circle-right"), style = "font-size:125%"),
           tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars2.githubusercontent.com/u/25057508?s=60&v=4", alt = "Card image"
-            ),
-            tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Shixiang Wang"),
-              tags$p(class = "card-text", "Some information displayed"),
-              tags$a(href = "#", class = "card-link", "See Profile")
-            )
+            id = "hubs_info",
+            style = "padding: 0px 5px 1px",
+            checkboxGroupInput("hubs_text", NULL, choiceNames = c("UCSC Public Hub", "TCGA Hub",
+                                                              "GDC Xena Hub", "ICGC Xena Hub",
+                                                              "Pan-Cancer Atlas Hub", "GA4GH (TOIL) Hub",
+                                                              "Treehouse Hub", "PCAWG Hub",
+                                                              "ATAC-seq Hub", "Singel Cell Xena hub"),
+                               choiceValues = c("publicHub", "tcgaHub", "gdcHub", "icgcHub", "pancanAtlasHub",
+                                                "toilHub", "treehouseHub", "pcawgHub", "atacseqHub", "singlecellHub"))
+          ),
+          
+          hr(),
+          actionLink("cohorts", "Cohorts Name :", icon = icon("arrow-circle-right"), style = "font-size:125%"),
+          tags$div(
+            id = "cohorts_info",
+            style = "padding: 8px 1px 0px",
+            textInput("cohorts_text", NULL, width = "80%", placeholder = "Breast", value = NULL)
+          ),
+          
+          hr(),
+          actionLink("subtype", "Data subtype :", icon = icon("arrow-circle-right"), style = "font-size:125%"),
+          tags$div(
+            id = "subtype_info",
+            style = "padding: 8px 1px 0px",
+            textInput("subtype_text", NULL, width = "80%", placeholder = "gene expression", value = NULL)
+          ),
+          
+          hr(),
+          actionLink("type", "Type :", icon = icon("arrow-circle-right"), style = "font-size:125%"),
+          tags$div(
+            id = "type_info",
+            style = "padding: 0px 5px 1px",
+            checkboxGroupInput("type_text", NULL, choices  = c("genomicMatrix", "clinicalMatrix", "genomicSegment", "mutationVector"))
           )
         ),
-        column(
-          4,
-          tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars3.githubusercontent.com/u/17489298?s=60&v=4", alt = "Card image"
-            ),
-            tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Fei Zhao"),
-              tags$p(class = "card-text", "Some information displayed"),
-              tags$a(href = "#", class = "card-link", "See Profile")
-            )
-          )
-        ),
-        column(
-          4,
-          tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars1.githubusercontent.com/u/28949856?s=60&v=4", alt = "Card image"
-            ),
-            tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Yi Xiong"),
-              tags$p(class = "card-text", "Some information displayed"),
-              tags$a(href = "#", class = "card-link", "See Profile")
-            )
-          )
+        
+        mainPanel(
+          DT::dataTableOutput("xena_table")
+          # verbatimTextOutput('x1')
         )
-      ),
-      tags$br(),
-      fluidRow(
-        class = "text-center center-block",
-        column(
-          4,
-          tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars3.githubusercontent.com/u/37660840?s=60&v=4", alt = "Card image"
-            ),
+      )
+      
+      # sidebarPanel(
+      #   textInput("txt", "Text input:", "text here"),
+      #   sliderInput("slider", "Slider input:", 1, 100, 30),
+      #   actionButton("action", "Button"),
+      #   actionButton("action2", "Button2", class = "btn-primary")
+      # ),
+      # mainPanel(DT::dataTableOutput("xena_table"))
+    ),
+    navbarMenu(
+      title = "Modules",
+      icon = icon("buromobelexperte"),
+      tabPanel("module 1"),
+      tabPanel("module 1"),
+      tabPanel("module 3")
+    ),
+    navbarMenu(
+      title = "Pipelines",
+      icon = icon("angle-double-down"),
+      tabPanel("pipeline 1"),
+      tabPanel("pipeline 2"),
+      tabPanel("pipeline 3")
+    ),
+    tabPanel(
+      title = "Developers",
+      icon = icon("user-friends"),
+      fluidPage(
+        # titlePanel("Developers"),
+        fluidRow(
+          class = "text-center center-block",
+          column(
+            4,
             tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Longfei Zhao"),
-              tags$p(class = "card-text", "Some information displayed")
-            ),
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars2.githubusercontent.com/u/25057508?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Shixiang Wang"),
+                tags$p(class = "card-text", "Some information displayed"),
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
+            )
+          ),
+          column(
+            4,
             tags$div(
-              class = "card-footer",
-              tags$a(href = "#", class = "card-link", "See Profile")
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars3.githubusercontent.com/u/17489298?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Fei Zhao"),
+                tags$p(class = "card-text", "Some information displayed"),
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
+            )
+          ),
+          column(
+            4,
+            tags$div(
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars1.githubusercontent.com/u/28949856?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Yi Xiong"),
+                tags$p(class = "card-text", "Some information displayed"),
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
             )
           )
         ),
-        column(
-          4,
-          tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars1.githubusercontent.com/u/38618580?s=60&v=4", alt = "Card image"
-            ),
+        tags$br(),
+        fluidRow(
+          class = "text-center center-block",
+          column(
+            4,
             tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Yin Li"),
-              tags$p(class = "card-text", "Some information displayed"),
-              tags$a(href = "#", class = "card-link", "See Profile")
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars3.githubusercontent.com/u/37660840?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Longfei Zhao"),
+                tags$p(class = "card-text", "Some information displayed")
+              ),
+              tags$div(
+                class = "card-footer",
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
             )
-          )
-        ),
-        column(
-          4,
-          tags$div(
-            class = "card bg-info text-dark center-block",
-            style = "width:400px",
-            tags$img(
-              class = "card-img-top  img-circle img-responsive center-block",
-              src = "https://avatars3.githubusercontent.com/u/22772592?s=60&v=4", alt = "Card image"
-            ),
+          ),
+          column(
+            4,
             tags$div(
-              class = "card-body",
-              tags$h4(class = "card-title", "Kai Gu"),
-              tags$p(class = "card-text", "Some information displayed"),
-              tags$a(href = "#", class = "card-link", "See Profile")
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars1.githubusercontent.com/u/38618580?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Yin Li"),
+                tags$p(class = "card-text", "Some information displayed"),
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
+            )
+          ),
+          column(
+            4,
+            tags$div(
+              class = "card bg-info text-dark center-block",
+              style = "width:400px",
+              tags$img(
+                class = "card-img-top  img-circle img-responsive center-block",
+                src = "https://avatars3.githubusercontent.com/u/22772592?s=60&v=4", alt = "Card image"
+              ),
+              tags$div(
+                class = "card-body",
+                tags$h4(class = "card-title", "Kai Gu"),
+                tags$p(class = "card-text", "Some information displayed"),
+                tags$a(href = "#", class = "card-link", "See Profile")
+              )
             )
           )
         )
       )
-    )
-  ),
-  footer = tags$footer(tags$a(href = "https://github.com/openbiox", "Openbiox"),
-    HTML(" &copy; "), tags$a(href = "https://github.com/openbiox/XenaShiny/blob/master/LICENSE", "MIT"),
-    align = "center", style = "
+    ),
+    footer = tags$footer(tags$a(href = "https://github.com/openbiox", "Openbiox"),
+                         HTML(" &copy; "), tags$a(href = "https://github.com/openbiox/XenaShiny/blob/master/LICENSE", "MIT"),
+                         align = "center", style = "
                             position:relative;
                             bottom:0;
                             width:100%;
                             height:50px;   /* Height of the footer */
                             padding: 10px;
                             z-index: 1000;"
-  ),
-  theme = shinythemes::shinytheme("cosmo")
+    ),
+    theme = shinythemes::shinytheme("cosmo")
+  )
 )
+
+
 
 # Server ------------------------------------------------------------------
 
 server <- function(input, output, session) {
+  observeEvent(input$hubs, {
+    toggle("hubs_info")
+  })
+  observeEvent(input$cohorts, {
+    toggle("cohorts_info")
+  })
+  observeEvent(input$subtype, {
+    toggle("subtype_info")
+  })
+  observeEvent(input$type, {
+    toggle("type_info")
+  })
+  
+  
+  observe({
+    s = input$xena_table_rows_selected
+    if (length(s)) {
+      showModal(
+        modalDialog(
+          title = "Detail information...",
+          size = "l",
+          DTOutput("detail_info"),
+          hr(),
+          tags$p(tags$span("Please click link to download: ", style = "font-size:110%; padding:0px 5px"), tags$a(href = url(), "Target dataset"))
+          
+        )
+      )
+    }
+  })
+  
+  dataset <- reactive({
+    res <- XenaData
+    if (!is.null(input$hubs_text)){
+      # print(input$hubs_text)
+      res <- dplyr::filter(res, XenaHostNames %in% input$hubs_text)
+    }
+    if (!is.null(input$cohorts_text)){
+      res <- dplyr::filter(res, grepl(input$cohorts_text, XenaCohorts, ignore.case = TRUE))
+    }
+    if (!is.null(input$subtype_text)){
+      res <- dplyr::filter(res, grepl(input$subtype_text, DataSubtype, ignore.case = TRUE))
+    }
+    if (!is.null(input$type_text)){
+      res <- dplyr::filter(res, grepl(input$type_text, Type, ignore.case = TRUE))
+    }
+    return(res)
+  })
+  
+  output$xena_table <- DT::renderDataTable(dataset()[,c("XenaDatasets", "XenaHostNames", "XenaCohorts","SampleCount", "DataSubtype", "Label")], selection = 'single')
+  
+  selected_database <- reactive({
+    s <- input$xena_table_rows_selected
+    if (length(s)) {
+      return(dataset()[s,])
+    }
+  })
+  
+  url <- reactive({
+    data <- selected_database()
+    xe <- XenaGenerate(subset = XenaDatasets == data$XenaDatasets)
+    xe_query = XenaQuery(xe)
+    return(xe_query$url)
+  })
+  
+  output$detail_info <- renderDT(selected_database(), options = list(dom = 't', scrollX = TRUE))
+  
   output$w <- renderText({
     req(input$side)
     r <- input$side
     paste("www", r)
   })
-  output$xena_table <- DT::renderDataTable(DT::datatable(xena_table))
 }
 
 
