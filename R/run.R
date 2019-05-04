484afd354774ed8b056c00b99445427a75095d2d
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-05-04 17:45:32 +0800

    👌 IMPROVE: test again

--- R/run.R
+++ R/run.R
@@ -5,29 +5,28 @@
 #########################################
 
 
-# Here data goes
-data("XenaData", package = "UCSCXenaTools", envir = environment())
-xena_table <- XenaData[, c(
-  "XenaDatasets", "XenaHostNames", "XenaCohorts",
-  "SampleCount", "DataSubtype", "Label"
-)]
-xena_table$SampleCount <- as.integer(xena_table$SampleCount)
-colnames(xena_table)[c(1:3)] <- c("Dataset ID", "Hub", "Cohort")
-
-data("dat_datasets", package = "UCSCXenaShiny", envir = environment())
-data("dat_samples", package = "UCSCXenaShiny", envir = environment())
-data("XenaInfo", package = "UCSCXenaShiny", envir = environment())
-
-
 #' Run Xena Shiny App
 #' 
 #' @importFrom shiny shinyAppFile
+#' @importFrom utils data
 #' @return NULL
 #' @export
 #'
 #' @examples 
 #' \donttest{app_run()}
 app_run <- function() {
+  # Here data goes
+  data("XenaData", package = "UCSCXenaTools", envir = environment())
+  xena_table <- XenaData[, c(
+    "XenaDatasets", "XenaHostNames", "XenaCohorts",
+    "SampleCount", "DataSubtype", "Label"
+  )]
+  xena_table$SampleCount <- as.integer(xena_table$SampleCount)
+  colnames(xena_table)[c(1:3)] <- c("Dataset ID", "Hub", "Cohort")
+  
+  data("dat_datasets", package = "UCSCXenaShiny", envir = environment())
+  data("dat_samples", package = "UCSCXenaShiny", envir = environment())
+  data("XenaInfo", package = "UCSCXenaShiny", envir = environment())
   shiny::shinyAppFile(system.file("shinyapp", "App.R", package = "UCSCXenaShiny"))
 }
 
@@ -35,20 +34,7 @@ app_run <- function() {
 # This is used for passing package checks
 #   Fix problem caused by 'no visible binding for global variable'
 
-# utils::globalVariables(
-#   c("Cohort",
-#     "DataSubtype",
-#     "DatasetCount",
-#     "Hub",
-#     "Percent",
-#     "SampleCount",
-#     "SampleCount_percent",
-#     "SampleCount_sum",
-#     "Sample_percent",
-#     "Type",
-#     "XenaCohorts",
-#     "XenaDatasets",
-#     "XenaHostNames",
-#     "N"
-#   )
-# )
+utils::globalVariables(
+  c("XenaData"
+  )
+)
