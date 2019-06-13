e97b073db30fc118fdf449cbf13eda702d001cfa
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-06-14 00:03:24 +0800

    finish get_pancan_value fun

--- R/ope_get_pancan_value.R
+++ R/ope_get_pancan_value.R
@@ -1,15 +1,30 @@
 # 获取某特征（基因、蛋白等）在Pancan中的表达量
 
-get_pancan_value <- function(identifier, data_subtype, host=available_hosts(), ) {
-  host = match.arg(host)
-  data_filter = XenaData %>%
-    dplyr::filter(XenaHostNames == host, DataSubtype == data_subtype)
-  host
-  
-  fetch_dense_values(host, dataset, identifiers = NULL, samples = NULL,
-                     check = TRUE, use_probeMap = FALSE)
-}
-
 available_hosts = function() {
   UCSCXenaTools:::.xena_hosts %>% as.character()
+}
+
+get_pancan_value <- function(identifier, subtype=NULL, dataset=NULL, host=available_hosts(), 
+                             samples = NULL) {
+  stopifnot(length(identifier) == 1)
+  host = match.arg(host)
+  
+  data = UCSCXenaTools::XenaData
+  if (!is.null(subtype)) {
+    data = data %>%
+      dplyr::filter(XenaHostNames == host, grepl(subtype, DataSubtype))
+  }
+  if (!is.null(dataset)) {
+    data = data %>% 
+      dplyr::filter(XenaHostNames == host, grepl(dataset, XenaDatasets))
+  }
+  
+  if (nrow(data) != 1) {
+    stop("More than one dataset is determined by input")
+  }
+  
+  res = fetch_dense_values(data[["XenaHosts"]], data[["XenaDatasets"]], 
+                           identifiers = identifier, samples = samples,
+                           check = FALSE, use_probeMap = TRUE)
+  as.numeric(res)
 }
