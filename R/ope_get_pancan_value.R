fefc12a6ad104a8fa19534420f65fd5e36b5e0c5
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-06-23 12:00:42 +0800

    doc get_pancan_value

--- R/ope_get_pancan_value.R
+++ R/ope_get_pancan_value.R
@@ -1,12 +1,46 @@
 # 获取某特征（基因、蛋白等）在Pancan中的表达量
 
+#' Show available hosts
+#'
+#' @return hosts
+#' @export
+#'
+#' @examples 
+#' available_hosts()
 available_hosts = function() {
   UCSCXenaTools:::.xena_hosts %>% as.character()
 }
 
+
+#' Fetch identifier value from pan-cancer dataset
+#'
+#' Identifier includes gene/probe etc.
+#' 
+#' @param identifier a length-1 character representing a gene symbol, ensembl gene id, or probe id. 
+#' Gene symbol is highly recommended.
+#' @param subtype a length-1 chracter representing a regular expression for matching 
+#' `DataSubtype` column of [UCSCXenaTools::XenaData].
+#' @param dataset a length-1 chracter representing a regular expression for matching 
+#' `XenaDatasets` of [UCSCXenaTools::XenaData].
+#' @param host a length-1 character representing host name, e.g. "toilHub".
+#' @param samples a character vector representing samples want to be returned.
+#'
+#' @return a named vector or `list`
+#'
+#' @examples
+#' # Fetch TP53 expression value from pan-cancer dataset
+#' \donttest{
+#' t1 = get_pancan_value("TP53", 
+#'            dataset = "TcgaTargetGtex_rsem_isoform_tpm", 
+#'            host = "toilHub")
+#' t2 = get_pancan_gene_value("TP53")
+#' }
+
+#' @export
+#' @describeIn get_pancan_value Fetch identifier value from pan-cancer dataset
 get_pancan_value <- function(identifier, subtype=NULL, dataset=NULL, host=available_hosts(), 
                              samples = NULL) {
-  stopifnot(length(identifier) == 1)
+  stopifnot(length(identifier) == 1, !all(is.null(subtype), is.null(dataset)))
   if (!requireNamespace("UCSCXenaTools", quietly = TRUE)) 
     stop("Package 'UCSCXenaTools' is not installed.")
   if (!"UCSCXenaTools" %in% .packages())
@@ -24,17 +58,19 @@ get_pancan_value <- function(identifier, subtype=NULL, dataset=NULL, host=availa
   }
   
   if (nrow(data) != 1) {
-    stop("More than one dataset is determined by input")
+    stop("No dataset or more than one dataset is determined by input")
   }
   
   res = fetch_dense_values(data[["XenaHosts"]], data[["XenaDatasets"]], 
                            identifiers = identifier, samples = samples,
                            check = FALSE, use_probeMap = TRUE)
-  res2 = res
+  res2 = res[1, ]
   names(res2) = colnames(res)
   res2
 }
 
+#' @describeIn get_pancan_value Fetch gene expression value from pan-cancer dataset
+#' @export
 get_pancan_gene_value <- function(identifier, host = "toilHub") {
   host = match.arg(host)
   if (host == "toilHub") {
