6de818ef7d6a92a7492a23e40b7abec819f25f3b
Yi Xiong <390479311@qq.com>
2020-06-18 13:43:04 +0800

    增加stemness, genome instability数据

--- data-raw/obtain_pancan_data.R
+++ data-raw/obtain_pancan_data.R
@@ -27,7 +27,31 @@ toil_surv <- XenaGenerate(subset = XenaDatasets == "TCGA_survival_data") %>%
 # tmb_data <- data.table::fread("data-raw/mutation-load_updated.txt", data.table = F)
 # names(tmb_data)[c(4,5)] <- c("Silent_per_Mb","Non_silent_per_Mb")
 
+#-----------Stemness----------------------------------------
+#access date:2020-06-17
+#from https://pancanatlas.xenahubs.net/download/StemnessScores_DNAmeth_20170210.tsv.gz
+# stemness_data <- data.table::fread("data-raw/StemnessScores_RNAexp_20170127.2.tsv",data.table = F)
+# stemness_data <- stemness_data %>% tibble::column_to_rownames(var = "sample") %>% t() %>% as.data.frame()
+# stemness_data_RNA <- stemness_data %>%
+#   tibble::rownames_to_column(var = "sample") %>%
+#   dplyr::mutate(sample = stringr::str_replace_all(sample,"\\.","-"))
+
+#--------purity and ploidy data-----------------------------
+#access date:2020-06-17
+#from https://gdc.cancer.gov/about-data/publications/PanCanStemness-2018
+##genome instability
+gi_data <- data.table::fread("data-raw/Purity_Ploidy_All_Samples_9_28_16.tsv",data.table = F)
+gi_data <- gi_data %>%
+  dplyr::select(c(3,5,6,7,9,10))
+gi_data <- gi_data %>%
+  dplyr::select(sample,purity,ploidy,Genome_doublings = `Genome doublings`,Cancer_DNA_fraction = `Cancer DNA fraction`,Subclonal_genome_fraction = `Subclonal genome fraction`) %>%
+  dplyr::mutate(sample = stringr::str_sub(sample,1,15))
+
+
+
 usethis::use_data(toil_info, overwrite = TRUE)
 usethis::use_data(toil_surv, overwrite = TRUE)
 usethis::use_data(immune_sig, overwrite = TRUE)
 usethis::use_data(tmb_data, overwrite = TRUE)
+usethis::use_data(stemness_data_RNA, overwrite = TRUE)
+usethis::use_data(gi_data, overwrite = TRUE)
