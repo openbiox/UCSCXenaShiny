8e4819d17da087e46e5846e89ef18a05f4697ef9
Gu <guk8@asia.roche.com>
2020-06-18 23:32:10 +0800

    shinyFiles指定数据download路径

--- inst/shinyapp/modules/modules-pancan-search.R
+++ inst/shinyapp/modules/modules-pancan-search.R
@@ -54,8 +54,6 @@ ui.modules_pancan_dist <- function(id) {
           h5("3. You have to turn on both 'Show P value' and 'Show P label' to show significant labels")
         ),width = 10
       )
-<<<<<<< HEAD
-=======
     ),
     fluidRow(
       materialSwitch(ns("pdist_mode"), "Show violin plot", inline = TRUE),
@@ -76,7 +74,6 @@ ui.modules_pancan_dist <- function(id) {
       h5("1. The data query may take some time based on your network. Wait until a plot shows"),
       h5("2. The unit of gene expression is log2(tpm+0.001)"),
       h5("3. You have to turn on both 'Show P value' and 'Show P label' to show significant labels")
->>>>>>> 5b36faccfb41fd66f54da84ac83564a98066119e
     )
   )
 }
