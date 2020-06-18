a5652c19fdedb1041c2c37009f41b098425640f4
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-06-18 12:48:10 +0800

    添加 pkgdown site

--- R/xenashiny.R
+++ R/xenashiny.R
@@ -1,5 +1,5 @@
 #' @title Xena Shiny App
-#' @description A Shiny App for UCSC Xena Data Hubs.
+#' @description A Shiny App for UCSC Xena Data Hubs. See <https://github.com/openbiox/UCSCXenaShiny> for details.
 #' @docType package
 #' @name UCSCXenaShiny
 NULL
