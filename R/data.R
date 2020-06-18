5dec8aae8d9f53d792b2b9ee6d5d0e2e372cdfcc
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-06-18 16:42:52 +0800

    :bookmark_tabs:  更新数据集文档

--- R/data.R
+++ R/data.R
@@ -61,73 +61,35 @@ NULL
 #' @format A [data.frame]
 #' @examples
 #' data("immune_sig")
-#' access date: 2020-06-14
-#' @source  https://gdc.cancer.gov/about-data/publications/panimmune
-#' \dontrun{
-#' require(data.table)
-#' immune_sig <- data.table::fread("data-raw/Scores_160_Signatures.tsv", data.table = F)
-#' #test = immune_sig[1:5,1:5]
-#' immune_sig = immune_sig %>% tibble::column_to_rownames("V1")
-#' }
+#' @source  <https://gdc.cancer.gov/about-data/publications/panimmune>
+NULL
 
-#' TCGA: TMB (tumor mutation burden)
+#' TCGA: TMB (tumor mutation burden) data
 #' @docType data
 #' @name  tmb_data
 #' @format A [data.frame]
 #' @examples
 #' data("tmb_data")
-#' access date: 2020-06-14
-#' @source https://gdc.cancer.gov/about-data/publications/panimmune
-#' \dontrun{
-#' require(data.table)
-#' tmb_data <- data.table::fread("data-raw/mutation-load_updated.txt", data.table = F)
-#' names(tmb_data)[c(4,5)] <- c("Silent_per_Mb","Non_silent_per_Mb")
-#' }
-#' 
+#' @source <https://gdc.cancer.gov/about-data/publications/panimmune>
+NULL
 
-#' TCGA: Stemness
+#' TCGA: Stemness data
 #' @docType data
 #' @name  stemness_data_RNA
 #' @format A [data.frame]
 #' @examples
 #' data("stemness_data_RNA")
-#' access date:2020-06-17
-#' @source https://pancanatlas.xenahubs.net/download/StemnessScores_DNAmeth_20170210.tsv.gz
-#' 
-#' \dontrun{
-#' stemness_data <- data.table::fread("data-raw/StemnessScores_RNAexp_20170127.2.tsv",data.table = F)
-#' stemness_data <- stemness_data %>% tibble::column_to_rownames(var = "sample") %>% t() %>% as.data.frame()
-#' stemness_data_RNA <- stemness_data %>%
-#   tibble::rownames_to_column(var = "sample") %>%
-#   dplyr::mutate(sample = stringr::str_replace_all(sample,"\\.","-"))
-#' }
+#' @source <https://pancanatlas.xenahubs.net/download/StemnessScores_DNAmeth_20170210.tsv.gz>
+NULL
 
-#' TCGA: purity and ploidy data
+#' TCGA: genome instability data
 #' @docType data
 #' @name  gi_data
 #' @format A [data.frame]
 #' @examples
 #' data("gi_data")
-#' access date:2020-06-17
-#' @source  https://gdc.cancer.gov/about-data/publications/PanCanStemness-2018
-#' genome instability
-#' \dontrun{
-#'  gi_data <-
-#'    data.table::fread("data-raw/Purity_Ploidy_All_Samples_9_28_16.tsv",
-#'                      data.table = F)
-#'  gi_data <- gi_data %>%
-#'    dplyr::select(c(3, 5, 6, 7, 9, 10))
-#'  gi_data <- gi_data %>%
-#'    dplyr::select(
-#'      sample,
-#'      purity,
-#'      ploidy,
-#'      Genome_doublings = `Genome doublings`,
-#'      Cancer_DNA_fraction = `Cancer DNA fraction`,
-#'      Subclonal_genome_fraction = `Subclonal genome fraction`
-#'    ) %>%
-#'    dplyr::mutate(sample = stringr::str_sub(sample, 1, 15))
-#' }
+#' @source <https://gdc.cancer.gov/about-data/publications/PanCanStemness-2018>
+NULL
 
 #' TCGA: purity data
 #' @docType data
@@ -135,13 +97,5 @@ NULL
 #' @format A [data.frame]
 #' @examples
 #' data("purity_data")
-#' access date:2020-06-18
-#' @source https://www.nature.com/articles/ncomms9971#Sec14
-#' \dontrun{
-#' library(readxl)
-#' purity_data <- read_excel("data-raw/41467_2015_BFncomms9971_MOESM1236_ESM.xlsx",skip = 3)
-#' purity_data = purity_data %>%
-#'  dplyr::select(c(1:7)) %>%
-#'  dplyr::rename(sample = "Sample ID",cancer_type = "Cancer type") %>%
-#'  dplyr::mutate(sample = stringr::str_sub(sample,1,15))
-#' }
+#' @source <https://www.nature.com/articles/ncomms9971#Sec14>
+NULL
