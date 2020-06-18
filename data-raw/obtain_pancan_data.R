566907703f740da7539058a10396c193a64fae0c
Yi Xiong <390479311@qq.com>
2020-06-18 13:54:49 +0800

    增加纯度数据

--- data-raw/obtain_pancan_data.R
+++ data-raw/obtain_pancan_data.R
@@ -47,6 +47,15 @@ gi_data <- gi_data %>%
   dplyr::select(sample,purity,ploidy,Genome_doublings = `Genome doublings`,Cancer_DNA_fraction = `Cancer DNA fraction`,Subclonal_genome_fraction = `Subclonal genome fraction`) %>%
   dplyr::mutate(sample = stringr::str_sub(sample,1,15))
 
+#-------purity data----------------------------------------
+#access date:2020-06-18
+#from https://www.nature.com/articles/ncomms9971#Sec14
+# library(readxl)
+# purity_data <- read_excel("data-raw/41467_2015_BFncomms9971_MOESM1236_ESM.xlsx",skip = 3)
+# purity_data = purity_data %>%
+#   dplyr::select(c(1:7)) %>%
+#   dplyr::rename(sample = "Sample ID",cancer_type = "Cancer type") %>%
+#   dplyr::mutate(sample = stringr::str_sub(sample,1,15))
 
 
 usethis::use_data(toil_info, overwrite = TRUE)
@@ -55,3 +64,4 @@ usethis::use_data(immune_sig, overwrite = TRUE)
 usethis::use_data(tmb_data, overwrite = TRUE)
 usethis::use_data(stemness_data_RNA, overwrite = TRUE)
 usethis::use_data(gi_data, overwrite = TRUE)
+usethis::use_data(purity_data, overwrite = TRUE)
