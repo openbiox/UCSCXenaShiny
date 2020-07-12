2e515d22d49008ce2595d0efa4938f85f062eacf
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-07-12 23:15:56 +0800

    获取 pancan 蛋白表达、基因突变状态 (#103)

--- R/get_pancan_value.R
+++ R/get_pancan_value.R
@@ -34,6 +34,8 @@ available_hosts <- function() {
 #'   host = "toilHub"
 #' )
 #' t2 <- get_pancan_gene_value("TP53")
+#' t3 <- get_pancan_protein_value("AKT")
+#' t4 <- get_pancan_mutation_status("TP53")
 #' }
 #'
 #' @export
@@ -100,13 +102,11 @@ try_query_value = function(host, dataset,
 
 #' @describeIn get_pancan_value Fetch gene expression value from pan-cancer dataset
 #' @export
-get_pancan_gene_value <- function(identifier, host = "toilHub") {
-  host <- match.arg(host)
-  if (host == "toilHub") {
-    dataset <- "TcgaTargetGtex_rsem_isoform_tpm"
-    expression <- get_pancan_value(identifier, dataset = dataset, host = host)
-    unit <- "log2(tpm+0.001)"
-  }
+get_pancan_gene_value <- function(identifier) {
+  host <- "toilHub"
+  dataset <- "TcgaTargetGtex_rsem_isoform_tpm"
+  expression <- get_pancan_value(identifier, dataset = dataset, host = host)
+  unit <- "log2(tpm+0.001)"
   msg <- paste0(
     "More info about dataset please run following commands:\n",
     "  library(UCSCXenaTools)\n",
@@ -117,6 +117,87 @@ get_pancan_gene_value <- function(identifier, host = "toilHub") {
   res
 }
 
+#' @describeIn get_pancan_value Fetch protein expression value from pan-cancer dataset
+#' @export
+get_pancan_protein_value <- function(identifier) {
+  ## NOTE: Only ~200 proteins available, so many identifiers will return NAs
+  host <- "pancanAtlasHub"
+  dataset <- "TCGA-RPPA-pancan-clean.xena"
+  expression <- get_pancan_value(identifier, dataset = dataset, host = host)
+  unit <- "norm_value"
+  msg <- paste0(
+    "More info about dataset please run following commands:\n",
+    "  library(UCSCXenaTools)\n",
+    "  XenaGenerate(subset = XenaDatasets == \"", dataset, "\") %>% XenaBrowse()"
+  )
+  message(msg)
+  res <- list(expression = expression, unit = unit)
+  res
+}
+
+## .p_dataset_field("https://pancanatlas.xenahubs.net", "TCGA-RPPA-pancan-clean.xena")
+.all_pancan_proteins <- c("ACC1", "ACC_pS79", "ACETYLATUBULINLYS40", "ACVRL1", "ADAR1", 
+                          "AKT", "AKT_pS473", "AKT_pT308", "ALPHACATENIN", "AMPKALPHA", 
+                          "AMPKALPHA_pT172", "ANNEXIN1", "ANNEXINVII", "AR", "ARAF", "ARAF_pS299", 
+                          "ARID1A", "ASNS", "ATM", "AXL", "BAD_pS112", "BAK", "BAP1C4", 
+                          "BAX", "BCL2", "BCL2A1", "BCLXL", "BECLIN", "BETACATENIN", "BID", 
+                          "BIM", "BRAF", "BRAF_pS445", "BRCA2", "BRD4", "CA9", "CABL", 
+                          "CASPASE3", "CASPASE7CLEAVEDD198", "CASPASE8", "CASPASE9", "CAVEOLIN1", 
+                          "CD20", "CD26", "CD31", "CD49B", "CDK1", "CDK1_pY15", "CHK1", 
+                          "CHK1_pS296", "CHK1_pS345", "CHK2", "CHK2_pT68", "CHROMOGRANINANTERM", 
+                          "CIAP", "CJUN_pS73", "CK5", "CKIT", "CLAUDIN7", "CMET", "CMET_pY1235", 
+                          "CMYC", "COG3", "COLLAGENVI", "COMPLEXIISUBUNIT30", "CRAF", "CRAF_pS338", 
+                          "CTLA4", "CYCLINB1", "CYCLIND1", "CYCLINE1", "CYCLINE2", "DIRAS3", 
+                          "DJ1", "DUSP4", "DVL3", "E2F1", "ECADHERIN", "EEF2", "EEF2K", 
+                          "EGFR", "EGFR_pY1068", "EGFR_pY1173", "EIF4E", "EIF4G", "ENY2", 
+                          "EPPK1", "ERALPHA", "ERALPHA_pS118", "ERCC1", "ERCC5", "ERK2", 
+                          "ETS1", "EZH2", "FASN", "FIBRONECTIN", "FOXM1", "FOXO3A", "FOXO3A_pS318S321", 
+                          "G6PD", "GAB2", "GAPDH", "GATA3", "GATA6", "GCN5L2", "GSK3ALPHABETA", 
+                          "GSK3ALPHABETA_pS21S9", "GSK3_pS9", "GYGGLYCOGENIN1", "GYS", 
+                          "GYS_pS641", "HER2", "HER2_pY1248", "HER3", "HER3_pY1289", "HEREGULIN", 
+                          "HIF1ALPHA", "HSP70", "IGF1R_pY1135Y1136", "IGFBP2", "INPP4B", 
+                          "IRF1", "IRS1", "JAB1", "JAK2", "JNK2", "JNK_pT183Y185", "KEAP1", 
+                          "KU80", "LCK", "LCN2A", "LDHA", "LDHB", "LKB1", "MACC1", "MAPK_pT202Y204", 
+                          "MEK1", "MEK1_pS217S221", "MIG6", "MITOCHONDRIA", "MRE11", "MSH2", 
+                          "MSH6", "MTOR", "MTOR_pS2448", "MYH11", "MYOSINIIA", "MYOSINIIA_pS1943", 
+                          "NAPSINA", "NCADHERIN", "NDRG1_pT346", "NF2", "NFKBP65_pS536", 
+                          "NOTCH1", "NRAS", "NRF2", "OXPHOSCOMPLEXVSUBUNITB", "P16INK4A", 
+                          "P21", "P27", "P27_pT157", "P27_pT198", "P38MAPK", "P38_pT180Y182", 
+                          "P53", "P62LCKLIGAND", "P63", "P70S6K1", "P70S6K_pT389", "P90RSK", 
+                          "P90RSK_pT359S363", "PAI1", "PARP1", "PARPAB3", "PARPCLEAVED", 
+                          "PAXILLIN", "PCADHERIN", "PCNA", "PDCD1", "PDCD4", "PDK1", "PDK1_pS241", 
+                          "PDL1", "PEA15", "PEA15_pS116", "PI3KP110ALPHA", "PI3KP85", "PKCALPHA", 
+                          "PKCALPHA_pS657", "PKCDELTA_pS664", "PKCPANBETAII_pS660", "PKM2", 
+                          "PR", "PRAS40_pT246", "PRDX1", "PREX1", "PTEN", "PYGB", "PYGBAB2", 
+                          "PYGL", "PYGM", "RAB11", "RAB25", "RAD50", "RAD51", "RAPTOR", 
+                          "RB", "RBM15", "RB_pS807S811", "RET_pY905", "RICTOR", "RICTOR_pT1135", 
+                          "S6", "S6_pS235S236", "S6_pS240S244", "SCD1", "SETD2", "SF2", 
+                          "SHC_pY317", "SHP2_pY542", "SLC1A5", "SMAC", "SMAD1", "SMAD3", 
+                          "SMAD4", "SNAIL", "SRC", "SRC_pY416", "SRC_pY527", "STAT3_pY705", 
+                          "STAT5ALPHA", "STATHMIN", "SYK", "SYNAPTOPHYSIN", "TAZ", "TFRC", 
+                          "THYMIDILATESYNTHASE", "TIGAR", "TRANSGLUTAMINASE", "TSC1", "TTF1", 
+                          "TUBERIN", "TUBERIN_pT1462", "VEGFR2", "X1433BETA", "X1433EPSILON", 
+                          "X1433ZETA", "X4EBP1", "X4EBP1_pS65", "X4EBP1_pT37T46", "X4EBP1_pT70", 
+                          "X53BP1", "XBP1", "XRCC1", "YAP", "YAP_pS127", "YB1", "YB1_pS102")
+
+#' @describeIn get_pancan_value Fetch mutation status value from pan-cancer dataset
+#' @export
+get_pancan_mutation_status <- function(identifier) {
+  if (utils::packageVersion("UCSCXenaTools") < "1.3.2") {
+    stop("You need to update 'UCSCXenaTools' (>=1.3.2).", call. = FALSE)
+  }
+  
+  host <- "pancanAtlasHub"
+  dataset <- "mc3.v0.2.8.PUBLIC.nonsilentGene.xena"
+  msg <- paste0(
+    "More info about dataset please run following commands:\n",
+    "  library(UCSCXenaTools)\n",
+    "  XenaGenerate(subset = XenaDatasets == \"", dataset, "\") %>% XenaBrowse()"
+  )
+  message(msg)
+  
+  get_pancan_value(identifier, dataset = dataset, host = host)
+}
 
 utils::globalVariables(
   c(
