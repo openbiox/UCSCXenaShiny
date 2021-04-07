758a987dfccee6f3118e86765c02a81f872d9849
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-04-07 18:13:17 +0800

    通过缓存来减少 query 次数

--- inst/shinyapp/App.R
+++ inst/shinyapp/App.R
@@ -142,20 +142,39 @@ server_file <- function(x) {
 
 
 # Set utility functions ---------------------------------------------------
-
-xe_query_url <- function(data) {
-  xe <-
-    UCSCXenaTools::XenaGenerate(subset = XenaDatasets %in% data$XenaDatasets)
-
-  xe_query <- UCSCXenaTools::XenaQuery(xe)
-  xe_query$browse <- purrr::map2(
-    xe_query$datasets, xe_query$hosts,
-    ~utils::URLencode(
-      paste0(
-        "https://xenabrowser.net/datapages/?",
-        "dataset=", .x, "&host=", .y
+QUERY_CACHE <- dplyr::tibble()
+xe_query_url <- function(data, use_cache = TRUE) {
+  
+  if (use_cache) {
+    if (nrow(QUERY_CACHE) == 0) {
+      non_exist_idx <- !data$XenaDatasets %in% NULL
+    } else {
+      non_exist_idx <- !data$XenaDatasets %in% QUERY_CACHE$datasets
+    }
+    if (any(non_exist_idx)) {
+      non_exist_query <- xe_query_url(data[non_exist_idx, , drop = FALSE], use_cache = FALSE)
+      QUERY_CACHE <<- dplyr::bind_rows(
+        QUERY_CACHE,
+        non_exist_query
       )
-    )) %>% unlist()
+    }
+    
+    xe_query <- dplyr::filter(QUERY_CACHE, QUERY_CACHE$datasets %in% data$XenaDatasets)
+  } else {
+    xe <-
+      UCSCXenaTools::XenaGenerate(subset = XenaDatasets %in% data$XenaDatasets)
+    
+    xe_query <- UCSCXenaTools::XenaQuery(xe)
+    xe_query$browse <- purrr::map2(
+      xe_query$datasets, xe_query$hosts,
+      ~utils::URLencode(
+        paste0(
+          "https://xenabrowser.net/datapages/?",
+          "dataset=", .x, "&host=", .y
+        )
+      )) %>% unlist()
+  }
+  
   return(xe_query)
 }
 
