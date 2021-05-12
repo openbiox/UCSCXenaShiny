bfd9502b5f83e23f39886bab955b4f31d591c9ed
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-05-12 18:45:40 +0800

    清理 pcawg promoter 的id并进行远程存储和获取

---
+++ data-raw/clean_promoter_id.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+all(probe$chromStart == probe$chromEnd)
+
+probe <- read_tsv("data-raw/expressedPromoterCoordinates.probeMap")
+probe$chrom <- gsub("chr", "", probe$chrom)
+
+df <- probe %>% 
+  mutate(loc_id = paste0(chrom, ":", chromStart, ":", gene)) %>% 
+  select(loc_id, id, strand) %>% 
+  mutate(strand = factor(strand))
+  
+
+df
+probeMap <- df$id
+names(probeMap) <- df$loc_id
+
+probeMap[names(probeMap) == "19:12203078:ZNF788"] # 存在极少数有2个promoter id相同。
+
+save(probeMap, file = "data-raw/pcawg_promoter_id.rda")
