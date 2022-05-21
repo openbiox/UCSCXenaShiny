a17b3f5db9bb3614324eddd5a9c3b67e7fab2eff
ShixiangWang <wangshx@shanghaitech.edu.cn>
2022-05-21 23:12:49 +0800

    typo

--- inst/shinyapp/ui/help.R
+++ inst/shinyapp/ui/help.R
@@ -36,6 +36,6 @@ ui.page_help <- function() {
     ),
     tabPanel(a("Feature overview", 
                href="https://oup.silverchair-cdn.com/oup/backfile/Content_public/Journal/bioinformatics/38/2/10.1093_bioinformatics_btab561/1/btab561_supplementary_data.pdf?Expires=1656169623&Signature=rJ6nWlQTfnLBq4O4r5CshbOoVclqyRN7jiibdgO7cGER8Fm-PCArYkzWp~oyhZxu7~VYdPtygAYjssguu9OsYi~OiSf5QCFA9S4Ws7BaNaxK7-oxqduZX9uK6I00IMHC4ED1jovLs3X44rx7Pk--RwFrYoAYV1u8M9UiiCTfTyX3JzySHqA0FDpi5MvKSPOwg5tqquCHUsg9pMB2uahyuhmyJ1R0n7YGhagmAT~nh0irTEtiEEgTrXtNRgcVTTUHsDvEl-QCU9jEssma0oxQxwoipeLlRWygOEPM1KffojCEvNetQDQnQqpePrDiGLkhqM3PkyUtnc62wwjkvhH1KA__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA",
-               target="_blank")),
+               target="_blank"))
   )
 }
