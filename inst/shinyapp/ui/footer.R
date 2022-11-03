3dc4a4a2822b8871c2cb739a1e2c1325ff02a158
ShixiangWang <wangshx@shanghaitech.edu.cn>
2022-11-04 00:27:40 +0800

    add revolvermaps in footer

--- inst/shinyapp/ui/footer.R
+++ inst/shinyapp/ui/footer.R
@@ -5,6 +5,7 @@ ui.footer <- function() {
     tags$br(),
     HTML("A community-driven bioinformatics innovation collaboration group in China |"),
     tags$a(href = "https://github.com/openbiox/XenaShiny/blob/master/LICENSE", "GPLv3 License"),
+    HTML('<script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=5arxfkv3006&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;f=lucida_console&amp;l=33" async="async"></script>'),
     align = "center", style = "
                            position:relative;
                            bottom:0;
