69c5ce04f79d974d1c371a59d20eac07c31c3899
lishensuo <80585985+lishensuo@users.noreply.github.com>
2023-07-29 16:35:39 +0800

    add mutation grouping comparison function and module (#262)

---
+++ R/vis_toil_Mut.R
@@ -0,0 +1,296 @@
+#' Visualize molecular profile difference between mutation and wild status of queried gene
+#'
+#' @param mut_Gene the queried gene to determine grouping based on mutation and wild status
+#' @param Gene a molecular identifier (e.g., "TP53") or a formula specifying
+#' genomic signature (`"TP53 + 2 * KRAS - 1.3 * PTEN"`).
+#' @param data_type choose gene profile type,
+#' including "mRNA", "transcript", "methylation", "miRNA".
+#' @param size_cutoff the minimum group size for a specific cancer
+#' @param Mode choose one visualize mode to represent data
+#' @param Show.P.value  `TRUE` or `FALSE` whether to count P value
+#' @param Show.P.label `TRUE` or `FALSE` present p value with number or label `*`, `**`, `***` and `****`
+#' @param Method default method is wilcox.test
+#' @param values the color to fill mutation or wild status
+#' @param draw_quantiles draw quantiles for violinplot
+#' @param trim whether to trim the violin
+#' @param plot Whether to return the plot or replace it with a p-value table
+#'
+#' @return a `ggplot` object or a tibble data.frame
+
+#'
+#' @examples
+#' \dontrun{
+#' p <- vis_toil_Mut(mut_Gene = "TP53")
+#' p <- vis_toil_Mut(mut_Gene = "TP53", Gene = "TNF")
+#' p <- vis_toil_Mut(mut_Gene = "TP53", Gene = "hsa-let-7d-3p", data_type = "miRNA")
+#' }
+#' @export
+
+vis_toil_Mut <- function(mut_Gene = "TP53", Gene = NULL, data_type = NULL,
+                         size_cutoff = 3, Mode = c("Boxplot", "Violinplot"),
+                         Show.P.value = TRUE, Show.P.label = TRUE,
+                         Method = c("wilcox.test", "t.test"),
+                         values = c("#DF2020", "#DDDF21"),
+                         draw_quantiles = c(0.25, 0.5, 0.75),
+                         trim = TRUE, plot = TRUE) {
+  Mode <- match.arg(Mode)
+  Method <- match.arg(Method)
+
+  # tumor samples grouping based on mutation
+  mut_dat_raw <- query_pancan_value(mut_Gene, data_type = "mutation")
+  if (all(is.na(mut_dat_raw))) {
+    stop("For the gene(", mut_Gene, ") mutation, all NAs returned.")
+  }
+  tcga_gtex <- load_data("tcga_gtex")
+  mut_dat <- mut_dat_raw %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("sample") %>%
+    dplyr::rename("mut" = ".") %>%
+    dplyr::inner_join(tcga_gtex, .) %>%
+    dplyr::filter(.data$type2 == "tumor") %>%
+    dplyr::select("sample", "tissue", "mut")
+  # affected molecular profile
+  if (is.null(Gene)) Gene <- mut_Gene
+  if (is.null(data_type)) data_type <- "mRNA"
+  if (!data_type %in% c("mRNA", "miRNA", "transcript", "methylation")) {
+    stop("data_type ", data_type, " does not support in this function!")
+  }
+  exp_dat_raw <- query_pancan_value(Gene, data_type = data_type)
+  if (all(is.na(exp_dat_raw$expression))) {
+    stop("For the gene(", Gene, ") profile, all NAs returned.")
+  }
+  unit <- exp_dat_raw$unit
+  exp_dat <- exp_dat_raw$expression %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("sample") %>%
+    dplyr::rename("tpm" = ".") %>%
+    dplyr::inner_join(tcga_gtex, .) %>%
+    dplyr::select("sample", "tpm")
+
+  # merge data
+  merge_dat <- dplyr::inner_join(mut_dat, exp_dat) %>%
+    dplyr::mutate(
+      tissue = as.character(.data$tissue),
+      mut = factor(.data$mut, levels = c(1, 0), labels  = c("Mutation", "Wild"))
+    )
+  group_stat <- table(merge_dat$tissue, merge_dat$mut)
+  tcga_discard <- rownames(group_stat)[apply(group_stat, 1, min) < size_cutoff]
+  if (length(tcga_discard) == length(group_stat)) {
+    stop("No one valid cancer type has enough grouping samples for comparsion.")
+  } else {
+    message(
+      paste(tcga_discard, collapse = " "), " was/were discarded due to less than ",
+      size_cutoff, " samples in one group."
+    )
+  }
+  merge_dat <- merge_dat %>%
+    dplyr::filter(!(.data$tissue %in% tcga_discard)) %>%
+    dplyr::mutate(mut = paste0(.data$mut, "(", mut_Gene, ")"))
+
+
+  # calculate p value significantce
+  tmp_dat <- merge_dat %>%
+    dplyr::group_by(.data$tissue, .data$mut) %>%
+    dplyr::summarise(tpm = mean(.data$tpm)) %>%
+    tidyr::pivot_wider(names_from = "mut", values_from = "tpm")
+
+  pv <- merge_dat %>%
+    ggpubr::compare_means(tpm ~ mut, data = ., method = Method, group.by = "tissue") %>%
+    dplyr::select(c("tissue", "p", "p.signif", "p.adj")) %>%
+    dplyr::mutate(Gene = Gene, .before = 1) %>%
+    dplyr::arrange(p) %>%
+    dplyr::inner_join(tmp_dat)
+
+  if (!plot) {
+    return(pv)
+  }
+
+  # visualization
+  if (Mode == "Boxplot") {
+    p <- ggplot2::ggplot(merge_dat, aes_string(x = "tissue", y = "tpm", fill = "mut")) +
+      ggplot2::geom_boxplot()
+  } else if (Mode == "Violinplot") {
+    p <- ggplot2::ggplot(
+      merge_dat, aes_string(x = "tissue", y = "tpm", fill = "mut")
+    ) +
+      geom_split_violin(
+        draw_quantiles = draw_quantiles,
+        trim = trim,
+        linetype = "solid",
+        color = "black",
+        size = 0.2,
+        na.rm = TRUE,
+        position = "identity"
+      )
+  }
+
+  p <- p +
+    ggplot2::xlab("") +
+    ggplot2::ylab(if (is.null(unit)) Gene else paste0(Gene, " (", unit, ")")) +
+    ggplot2::scale_fill_manual(values = values) +
+    ggplot2::theme_set(ggplot2::theme_classic(base_size = 20)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = .5, vjust = .5)) +
+    ggplot2::guides(fill = ggplot2::guide_legend(title = NULL)) +
+    ggplot2::theme(
+      legend.background = ggplot2::element_blank(),
+      legend.position = c(0, 0), legend.justification = c(0, 0)
+    )
+
+  if (Show.P.value == TRUE & Show.P.label == TRUE) {
+    p <- p + ggplot2::geom_text(
+      aes(
+        x = .data$tissue,
+        y = max(merge_dat$tpm) * 1.1,
+        label = .data$p.signif
+      ),
+      data = pv,
+      inherit.aes = FALSE
+    )
+  }
+
+  if (Show.P.value == TRUE & Show.P.label == FALSE) {
+    p <- p + ggplot2::geom_text(
+      aes(
+        x = .data$tissue,
+        y = max(merge_dat$tpm) * 1.1,
+        label = as.character(signif(.data$p, 2))
+      ),
+      data = pv,
+      inherit.aes = FALSE
+    )
+  }
+
+  return(p)
+}
+
+
+
+
+#' Visualize molecular profile difference between mutation and wild status of queried gene in Single Cancer Type
+#' @inheritParams vis_toil_Mut
+#' @param Cancer select cancer cohort(s).
+#' @return a `ggplot` object or a tibble data.frame.
+#' @export
+#'
+vis_toil_Mut_cancer <- function(mut_Gene = "TP53", Gene = NULL, data_type = NULL,
+                                size_cutoff = 3, Mode = c("Dotplot", "Violinplot"),
+                                Show.P.value = TRUE, Show.P.label = TRUE,
+                                Method = c("wilcox.test", "t.test"),
+                                values = c("#DF2020", "#DDDF21"),
+                                draw_quantiles = c(0.25, 0.5, 0.75),
+                                trim = TRUE, plot = TRUE, Cancer = "ACC") {
+  Mode <- match.arg(Mode)
+  Method <- match.arg(Method)
+
+  # tumor samples grouping based on mutation
+  mut_dat_raw <- query_pancan_value(mut_Gene, data_type = "mutation")
+  if (all(is.na(mut_dat_raw))) {
+    stop("For the gene(", mut_Gene, ") mutation, all NAs returned.")
+  }
+  tcga_gtex <- load_data("tcga_gtex")
+  mut_dat <- mut_dat_raw %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("sample") %>%
+    dplyr::rename("mut" = ".") %>%
+    dplyr::inner_join(tcga_gtex, .) %>%
+    dplyr::filter(.data$type2 == "tumor") %>%
+    dplyr::select("sample", "tissue", "mut")
+  # affected molecular profile
+  if (is.null(Gene)) Gene <- mut_Gene
+  if (is.null(data_type)) data_type <- "mRNA"
+  if (!data_type %in% c("mRNA", "miRNA", "transcript", "methylation")) {
+    stop("data_type ", data_type, " does not support in this function!")
+  }
+  exp_dat_raw <- query_pancan_value(Gene, data_type = data_type)
+  if (all(is.na(exp_dat_raw$expression))) {
+    stop("For the gene(", Gene, ") profile, all NAs returned.")
+  }
+  unit <- exp_dat_raw$unit
+  exp_dat <- exp_dat_raw$expression %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("sample") %>%
+    dplyr::rename("tpm" = ".") %>%
+    dplyr::inner_join(tcga_gtex, .) %>%
+    dplyr::select("sample", "tpm")
+
+  # merge data
+  merge_dat <- dplyr::inner_join(mut_dat, exp_dat) %>%
+    dplyr::mutate(
+      tissue = as.character(.data$tissue),
+      mut = factor(.data$mut, levels = c(1, 0), labels  = c("Mutation", "Wild"))
+    ) %>%
+    dplyr::filter(.data$tissue == Cancer) %>%
+    dplyr::mutate(mut = paste0(.data$mut, "(", mut_Gene, ")"))
+
+
+  group_stat <- table(merge_dat$mut)
+  if (min(group_stat) < size_cutoff) {
+    stop("The cancer type does not has enough grouping samples for comparsion.")
+  }
+
+
+  # calculate p value significantce
+  tmp_dat <- merge_dat %>%
+    dplyr::group_by(.data$tissue, .data$mut) %>%
+    dplyr::summarise(tpm = mean(.data$tpm)) %>%
+    tidyr::pivot_wider(names_from = "mut", values_from = "tpm")
+
+  pv <- merge_dat %>%
+    ggpubr::compare_means(tpm ~ mut, data = ., method = Method, group.by = "tissue") %>%
+    dplyr::select(c("tissue", "p", "p.signif")) %>%
+    dplyr::mutate(Gene = Gene, .before = 1) %>%
+    dplyr::arrange(p) %>%
+    dplyr::inner_join(tmp_dat)
+
+  if (!plot) {
+    return(pv)
+  }
+
+  if (Mode == "Dotplot") {
+    p <- ggpubr::ggdotplot(
+      merge_dat,
+      x = "mut", y = "tpm", fill = "mut", color = "mut", size = 0.6, binwidth = 0.2
+    )
+  } else if (Mode == "Violinplot") {
+    p <- ggplot2::ggplot(
+      merge_dat,
+      aes_string(x = "mut", y = "tpm", fill = "mut")
+    ) +
+      ggplot2::geom_violin(trim = FALSE) +
+      ggplot2::geom_boxplot(width = 0.1, fill = "white")
+  }
+  p <- p +
+    ggplot2::xlab(NULL) +
+    ggplot2::ylab(if (is.null(unit)) Gene else paste0(Gene, " (", unit, ")")) +
+    ggplot2::ggtitle(paste0("TCGA: ", Cancer)) +
+    theme_set(theme_classic(base_size = 20)) +
+    ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = .5, vjust = .5, size = 20)) +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_fill_manual(values = values) +
+    ggplot2::scale_color_manual(values = values)
+
+  if (Show.P.value == TRUE & Show.P.label == TRUE) {
+    p <- p + ggplot2::geom_text(
+      aes(
+        x = 1.5,
+        y = max(merge_dat$tpm) * 1.1,
+        label = .data$p.signif
+      ),
+      data = pv,
+      inherit.aes = FALSE
+    )
+  }
+
+  if (Show.P.value == TRUE & Show.P.label == FALSE) {
+    p <- p + ggplot2::geom_text(
+      aes(
+        x = 1.5,
+        y = max(merge_dat$tpm) * 1.1,
+        label = as.character(signif(.data$p, 2))
+      ),
+      data = pv,
+      inherit.aes = FALSE
+    )
+  }
+  return(p)
+}
---
+++ inst/shinyapp/modules/modules-pancan-mut.R
@@ -0,0 +1,213 @@
+ui.modules_pancan_mut = function(id){
+	ns = NS(id)
+	fluidRow(
+		column(
+			3,
+			wellPanel(
+	            selectizeInput(
+	              inputId = ns("mut_Gene"),
+	              label = "Input a gene with mutation ralated grouping",
+	              choices = pancan_identifiers$gene,
+	              selected = "TP53"
+	            ),
+	            shinyWidgets::prettyRadioButtons(
+	              inputId = ns("Mode"), label = "Select analysis cancer(s):",
+	              choiceValues = c("Pan-cancer", "Single-cancer"),
+	              choiceNames = c("Pan-cancer", "Single-cancer"),
+	              animation = "jelly"
+	            ),
+				tabsetPanel(
+				  id = ns("Mode_params"),
+				  type = "hidden",
+				  tabPanel("Single-cancer",
+				  	selectInput(ns("Cancer"), "Select one cancer",sort(tcga_cancer_choices))
+				  ),
+				  tabPanel("Pan-cancer")
+				),
+		        numericInput(inputId = ns("size_cutoff"), label = "Minimum group size", value = 3),
+			),
+			wellPanel(
+	            shinyWidgets::prettyRadioButtons(
+	              inputId = ns("profile"), label = "Select affected genomic profile:",
+	              choiceValues = c("mRNA", "transcript", "methylation", "miRNA"),
+	              choiceNames = c("mRNA Expression", "Transcript Expression", "DNA Methylation", "miRNA Expression"),
+	              animation = "jelly"
+	            ),
+	            selectizeInput(
+	              inputId = ns("Pancan_search"),
+	              label = "Input affected gene or formula (as signature)",
+	              choices = NULL,
+	              width = "100%",
+	              options = list(
+	                create = TRUE,
+	                maxOptions = 5,
+	                placeholder = "Enter a gene symbol, e.g. TP53",
+	                plugins = list("restore_on_backspace")
+	              )
+	        	),
+			),
+			wellPanel(
+		        materialSwitch(ns("pdist_mode"), "Show violin plot", inline = FALSE),
+		        materialSwitch(ns("pdist_show_p_value"), "Show P value", inline = TRUE),
+		        materialSwitch(ns("pdist_show_p_label"), "Show P label", inline = TRUE),
+		        colourpicker::colourInput(inputId = ns("mut_col"), "Mutated sample color", "#DF2020"),
+		        colourpicker::colourInput(inputId = ns("wild_col"), "Wild sample color", "#DDDF21"),
+		        selectInput(inputId = ns("theme"), label = "Select theme for plot", choices = names(themes_list), selected = "cowplot"),
+		        tags$hr(style = "border:none; border-top:2px solid #5E81AC;"),
+		        shinyWidgets::actionBttn(
+		          inputId = ns("plot_bttn"),
+		          label = "Go!",
+		          style = "gradient",
+		          icon = icon("search"),
+		          color = "primary",
+		          block = TRUE,
+		          size = "sm"
+		        )
+			),
+			wellPanel(
+		        numericInput(inputId = ns("height"), label = "Height", value = 5),
+		        numericInput(inputId = ns("width"), label = "Width", value = 12),
+		        prettyRadioButtons(
+		          inputId = ns("device"),
+		          label = "Choose plot format",
+		          choices = c("pdf", "png"),
+		          selected = "pdf",
+		          inline = TRUE,
+		          icon = icon("check"),
+		          animation = "jelly",
+		          fill = TRUE
+		        ),
+		        downloadBttn(
+		          outputId = ns("download"),
+		          style = "gradient",
+		          color = "default",
+		          block = TRUE,
+		          size = "sm"
+		        )
+			)
+		),
+		column(
+		    9,
+		    plotOutput(ns("mut_plot"), height = "600px",width = "600px"),
+		    hr(),
+		    h5("NOTEs:"),
+		    p("1. 500 common patwhay genesets from 3 resources(50 HALLMARK, 186 KEGG, 264 IOBR) were collected."),
+		    p("2. Pathway scores of TCGA(toil) tumor patients were calculated using ssGSEA method."),
+		    DT::DTOutput(ns("mut_data")),
+	        shinyjs::hidden(
+	          wellPanel(
+	            id = ns("save_csv"),
+	            downloadButton(ns("downloadTable"), "Save as csv")))
+
+
+		)
+	)
+
+}
+
+server.modules_pancan_mut = function(input, output, session){
+	ns = session$ns
+
+	observeEvent(input$Mode, {
+	  updateTabsetPanel(inputId = "Mode_params", selected = input$Mode)
+	})
+
+	profile_choices <- reactive({
+	  switch(input$profile,
+	    mRNA = list(all = pancan_identifiers$gene, default = "TP53"),
+	    methylation = list(all = pancan_identifiers$gene, default = "TP53"),
+	    transcript = list(all = load_data("transcript_identifier"), default = "ENST00000000233"),
+	    miRNA = list(all = pancan_identifiers$miRNA, default = "hsa-miR-769-3p"),
+	    list(all = "NONE", default = "NONE")
+	  )
+	})
+
+	observe({
+	  updateSelectizeInput(
+	    session,
+	    "Pancan_search",
+	    choices = profile_choices()$all,
+	    selected = profile_choices()$default,
+	    server = TRUE
+	  )
+	})
+	colors <- reactive({
+	  c(input$mut_col, input$wild_col)
+	})
+
+	plot_theme <- reactive({
+	  themes_list[[input$theme]]
+	})
+
+	w <- waiter::Waiter$new(id = ns("mut_plot"), html = waiter::spin_hexdots(), color = "white")
+
+	plot_func = eventReactive(input$plot_bttn, {
+		p = switch(input$Mode,
+			`Pan-cancer`=vis_toil_Mut(mut_Gene = input$mut_Gene, Gene = input$Pancan_search,
+				data_type=input$profile, size_cutoff=input$size_cutoff, Mode =ifelse(input$pdist_mode,"Violinplot", "Boxplot"),
+				Show.P.value = input$pdist_show_p_value, Show.P.label = input$pdist_show_p_label,
+				values = colors()
+				),
+			`Single-cancer`=vis_toil_Mut_cancer(mut_Gene = input$mut_Gene, Gene = input$Pancan_search,
+				data_type=input$profile, size_cutoff=input$size_cutoff, Mode =ifelse(input$pdist_mode,"Violinplot", "Dotplot"),
+				Show.P.value = input$pdist_show_p_value, Show.P.label = input$pdist_show_p_label,
+				values = colors(), Cancer=input$Cancer
+				)
+		)
+	  return(p)
+	})
+
+	data_func = eventReactive(input$plot_bttn, {
+		p_dat = switch(input$Mode,
+			`Pan-cancer`=vis_toil_Mut(mut_Gene = input$mut_Gene, Gene = input$Pancan_search,
+				data_type=input$profile, size_cutoff=input$size_cutoff, Mode =ifelse(input$pdist_mode,"Violinplot", "Boxplot"),
+				Show.P.value = input$pdist_show_p_value, Show.P.label = input$pdist_show_p_label,
+				values = colors(), plot = FALSE
+				),
+			`Single-cancer`=vis_toil_Mut_cancer(mut_Gene = input$mut_Gene, Gene = input$Pancan_search,
+				data_type=input$profile, size_cutoff=input$size_cutoff, Mode =ifelse(input$pdist_mode,"Violinplot", "Boxplot"),
+				Show.P.value = input$pdist_show_p_value, Show.P.label = input$pdist_show_p_label,
+				values = colors(), Cancer=input$Cancer, plot = FALSE
+				)
+		)
+	  return(p_dat)
+	})
+
+	output$mut_plot <- renderPlot({
+	  w$show() # Waiter add-ins
+	  plot_func()
+	})
+	output$mut_data <- renderDT({
+	  data_func()
+	})
+	output$download <- downloadHandler(
+	  filename = function() {
+	    paste0(input$Pancan_search,"_", input$profile,"_",input$mut_Gene,"_mutation.", input$device)
+	  },
+	  content = function(file) {
+	    p <- plot_func()
+	    if (input$device == "pdf") {
+	      pdf(file, width = input$width, height = input$height)
+	      print(p)
+	      dev.off()
+	    } else {
+	      png(file, width = input$width, height = input$height, res = 600, units = "in")
+	      print(p)
+	      dev.off()
+	    }
+	  }
+	)
+	observeEvent(input$plot_bttn, {
+		shinyjs::show(id = "save_csv")
+	})
+	output$downloadTable <- downloadHandler(
+	  filename = function() {
+	    paste0(input$Pancan_search,"_", input$profile,"_",input$mut_Gene,"_mutation.csv")
+	  },
+	  content = function(file) {
+	    write.csv(data_func(), file, row.names = FALSE)
+	  }
+	)
+
+}
+
