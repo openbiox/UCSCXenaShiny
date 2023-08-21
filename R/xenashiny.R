03d58c0989b606e85b28b18ef74712416a3865bd
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2023-08-21 09:54:36 +0800

    update pkg doc following the new CRAN feature
    Check https://github.com/r-lib/roxygen2/issues/1491. Thanks to K from CRAN team.

--- R/xenashiny.R
+++ R/xenashiny.R
@@ -1,9 +1,14 @@
 #' @title Xena Shiny App
 #' @description A Shiny App for UCSC Xena Data Hubs. See <https://github.com/openbiox/UCSCXenaShiny> for details.
-#' @docType package
-#' @name UCSCXenaShiny
+#' @keywords internal 
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
 #' @import ggplot2
 #' @importFrom psych corr.test
 #' @importFrom utils download.file head install.packages tail
 #' @importFrom stats prcomp predict quantile na.omit
+## usethis namespace: end
 NULL
