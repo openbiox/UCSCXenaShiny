ffd6fc7bf49b67621f4719dcc92801c440be7cf0
lishensuo <lishensuo@163.com>
2024-03-25 13:57:52 +0800

    Update vis_identifier.R

--- R/vis_identifier.R
+++ R/vis_identifier.R
@@ -255,12 +255,13 @@ vis_identifier_grp_comparison <- function(dataset = NULL, id = NULL, grp_df, sam
 
   if (do_grp) {
     fun <- if (fun_type == "betweenstats") ggstatsplot::grouped_ggbetweenstats else ggstatsplot::grouped_ggwithinstats
-    colnames(df)[4] = "grouping.var"
+    # colnames(df)[4] = "grouping.var"
     p <- fun(
       data = df,
       x = !!colnames(df)[3],
       y = !!colnames(df)[2],
-      grouping.var = grouping.var,
+      # grouping.var = grouping.var,
+      grouping.var = !!rlang::sym(colnames(df)[4]),
       type  = type,
       pairwise.comparisons = pairwise.comparisons,
       p.adjust.method = p.adjust.method,
